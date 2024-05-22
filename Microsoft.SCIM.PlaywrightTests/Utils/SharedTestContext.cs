03e55ef8e901d941f898055f060bb3d4856cbb3d
Joakim Järvinen <joakim.jarvinen@brightlyworks.com>
2024-05-22 21:08:31 +0300

    test: add User+Group garbage tests

--- Microsoft.SCIM.PlaywrightTests/Utils/SharedTestContext.cs
+++ Microsoft.SCIM.PlaywrightTests/Utils/SharedTestContext.cs
@@ -5,7 +5,13 @@ public static class SharedTestContext
     public static string? User2 { get; set; }
     public static string? User3 { get; set; }
     public static string? User4 { get; set; }
+    public static string? UserOMalley { get; set; }
+    public static string? UserEmp1 { get; set; }
+    public static string? UserEmp2 { get; set; }
+    public static string? UserEmp3 { get; set; }
+    public static string? UserEnterprise { get; set; }
     public static string? Group1 { get; set; }
     public static string? Group2 { get; set; }
     public static string? Group3 { get; set; }
+    public static string? Group1Garbage { get; set; }
 }
