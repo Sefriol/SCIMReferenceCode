9c064202e96bce7ce8e32cadf9ab295efc65970d
Joakim Järvinen <joakim.jarvinen@brightlyworks.com>
2024-05-22 13:53:14 +0300

    test: add GroupTests

--- Microsoft.SCIM.PlaywrightTests/Utils/JsonLoader.cs
+++ Microsoft.SCIM.PlaywrightTests/Utils/JsonLoader.cs
@@ -14,6 +14,25 @@ public class JsonLoader
         return jsonContent;
     }
 
+    public static async Task<string> LoadJsonDataAsync(string filePath, string[] variables, string[] values)
+    {
+        if (variables.Length != values.Length)
+        {
+            throw new ArgumentException("The number of variables and values must be the same.");
+        }
+
+        // Read the content of the JSON file
+        string jsonContent = await File.ReadAllTextAsync(filePath);
+        // Replace the placeholder with the actual value
+        for (int i = 0; i < variables.Length; i++)
+        {
+            jsonContent = jsonContent.Replace("{{" + variables[i] + "}}", values[i]);
+        }
+
+        // Parse the JSON content into a dynamic object
+        return jsonContent;
+    }
+
     public static async Task<string> LoadJsonDataAsync(string filePath)
     {
         // Read the content of the JSON file
